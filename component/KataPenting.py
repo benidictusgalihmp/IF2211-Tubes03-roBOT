3482224fae722768248c076abd31f3021a610bd0
naufalizza <mail.naufalizza@gmail.com>
2021-04-27 23:56:49 +0700

    fix KataPenting.py

--- component/KataPenting.py
+++ component/KataPenting.py
@@ -1,7 +1,5 @@
 import json
 class KataPenting():
-    
-    FILEPATH = "../data/"
     list_kata_penting = []
     # 
 
@@ -9,12 +7,11 @@ class KataPenting():
     # contoh akses: list_kata_indikator[0]["kw"][2] ; len(list_kata_indikator[0]["pre"][2]) == 0
 
     # DONE
-    def init():
-        # baca data dari file, tambahkan ke KataPenting.list_kata
-        with open(KataPenting.FILEPATH+"kata_penting.txt", "r") as f:
-            KataPenting.list_kata_penting = f.read().split('\n')
-        with open(KataPenting.FILEPATH+"kata_indikator.txt", "r") as f:
-            data = f.read()
-            KataPenting.list_kata_indikator = json.loads(data)
-
-    KataPenting.init()
+    FILEPATH = "./data/"
+    # baca data dari file, tambahkan ke KataPenting.list_kata
+    with open(FILEPATH+"kata_penting.txt", "r") as f:
+        list_kata_penting = f.read().split('\n')
+    with open(FILEPATH+"kata_indikator.json", "r") as f:
+        data = f.read()
+        list_kata_indikator = json.loads(data)
+    
--- lab.py
+++ lab.py
@@ -1,10 +1,13 @@
-import re
+# import re
 
-string = "IF2240 tubes basis data 22/04/2021"
-regex = "(\w{2}\d{4})|(tubes)|(tucil)|(\d{2}/\d{2}/\d{2})"
+# string = "IF2240 tubes basis data 22/04/2021"
+# regex = "(\w{2}\d{4})|(tubes)|(tucil)|(\d{2}/\d{2}/\d{2})"
 
-print(re.findall(regex, string))
+# print(re.findall(regex, string))
 
-txt = "The rain in Spain"
-x = re.findall("^The.*Spain$", txt) 
-print(x)
+# txt = "The rain in Spain"
+# x = re.findall("^The.*Spain$", txt) 
+# print(x)
+from component.KataPenting import KataPenting as kp
+
+print(kp.list_kata_indikator)
